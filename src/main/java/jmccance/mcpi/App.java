8951ff1c131ae9042d325f4c42c60af24e19cdc3
jmccance <jmccance@gmail.com>
2013-08-20 14:11:28 -0500

    Added support for different app modes. At the moment only console is implemented.

--- src/main/java/jmccance/mcpi/App.java
+++ src/main/java/jmccance/mcpi/App.java
@@ -8,7 +8,29 @@ import jmccance.mcpi.model.PiFinder;
  */
 public class App {
 
+  private static final String MODE_PROP = "mode";
+  private static final String CONSOLE_MODE_STR = "console";
+  private static final String SWING_MODE_STR = "swing";
+  private static final String DEFAULT_MODE_STR = SWING_MODE_STR;
+
+  private static enum AppMode {
+    CONSOLE,
+    SWING
+  }
+
   public static void main(String... args) {
+    switch (getAppMode()) {
+      case CONSOLE:
+        consoleApp();
+        break;
+
+      case SWING:
+      default:
+        swingApp();
+    }
+  }
+
+  private static void consoleApp() {
     final PiFinder finder = new PiFinder();
 
     int i = 1;
@@ -17,6 +39,21 @@ public class App {
       System.out.printf("%9d\t%1.8f\t%1.8f\n", i, finder.getApprox(), error);
       finder.refine();
       ++i;
+    }
+  }
+
+  private static void swingApp() {
+
+  }
+
+  private static AppMode getAppMode() {
+    switch (System.getProperty(MODE_PROP, DEFAULT_MODE_STR)) {
+      case CONSOLE_MODE_STR:
+        return AppMode.CONSOLE;
+
+      case SWING_MODE_STR:
+      default:
+        return AppMode.SWING;
     }
   }
 
